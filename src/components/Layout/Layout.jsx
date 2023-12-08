457958ff77584def51a91b30ef97ba4b9b1d3e8d
DmytroSkoryk <114485103+DmytroSkoryk@users.noreply.github.com>
2023-12-08 22:59:03 +0200

    header

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -4,39 +4,42 @@ import css from "./Layout.module.scss";
 const Loyout = () => {
   return (
     <div className={css.container}>
-      <header className={css.headerContainer}>
-        <NavLink to="/" className={css.logoContainer}>
-          <svg width="28" height="28">
-            <use href="../../../public/icons.svg#logo"></use>
-          </svg>
-          <p className={css.siteName}>Drink Master</p>
-        </NavLink>
-        <nav>
-          <NavLink to="/" className={css.pages}>
-            Home
+      <div className={css.headerWrapper}>
+        <header className={css.headerContainer}>
+          <NavLink to="/" className={css.logoContainer}>
+            <svg width="28" height="28">
+              <use href="../../../public/icons.svg#logo"></use>
+            </svg>
+            <p className={css.siteName}>Drink Master</p>
           </NavLink>
-          <NavLink to="/drinks" className={css.pages}>
-            Drinks
-          </NavLink>
-          <NavLink to="/add" className={css.pages}>
-            Add recipe
-          </NavLink>
-          <NavLink to="/my" className={css.pages}>
-            My recipes
-          </NavLink>
-          <NavLink to="/favorites" className={css.pages}>
-            Favorites
-          </NavLink>
-        </nav>
-        <div>
-          <img
-            src="../../../public/user.png"
-            alt="user"
-            className={css.userPhoto}
-          />
-          <div className={css.pages}>name</div>
-        </div>
-      </header>
+
+          <nav className={css.navContainer}>
+            <NavLink to="/" className={css.pages}>
+              Home
+            </NavLink>
+            <NavLink to="/drinks" className={css.pages}>
+              Drinks
+            </NavLink>
+            <NavLink to="/add" className={css.pages}>
+              Add recipe
+            </NavLink>
+            <NavLink to="/my" className={css.pages}>
+              My recipes
+            </NavLink>
+            <NavLink to="/favorites" className={css.pages}>
+              Favorites
+            </NavLink>
+          </nav>
+          <div className={css.userContainer}>
+            <img
+              src="../../../public/user.png"
+              alt="user"
+              className={css.userPhoto}
+            />
+            <div className={css.pages}>name</div>
+          </div>
+        </header>
+      </div>
 
       <div>
         <Outlet />
